0e4fc8fbdd700cb6bb79230cff65579bbe557cc4
vbetsch <71458556+vbetsch@users.noreply.github.com>
2025-07-20 22:39:48 +0200

    feat: Create endpoints for hello world page (#11)
    * feat: Create GET /hello
    
    * feat: Create GET /users
    
    * feat: Add migration
    
    * feat: Create lib prisma
    
    * feat: Get all credentials
    
    * feat: Get all master accounts

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,15 @@
+import {NextResponse} from 'next/server'
+import prisma from "../../../../lib/prisma";
+
+export async function GET() {
+    try {
+        const users = await prisma.masterAccount.findMany()
+        return NextResponse.json(users)
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json(
+            {message: 'An error has occurred.'},
+            {status: 500}
+        )
+    }
+}
