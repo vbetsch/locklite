6984225fa598a5e6a097df743eccacdc8e9cab9c
vbetsch <vic.betsch@gmail.com>
2025-07-20 22:33:15 +0200

    feat: Get all credentials

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,15 @@
-import { NextResponse } from 'next/server'
+import {NextResponse} from 'next/server'
+import prisma from "../../../../lib/prisma";
 
 export async function GET() {
-  const data = { users: ['exemple', 'fou', 'barre']}
-  return NextResponse.json(data)
+    try {
+        const users = await prisma.credential.findMany()
+        return NextResponse.json(users)
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json(
+            {message: 'An error has occurred.'},
+            {status: 500}
+        )
+    }
 }
