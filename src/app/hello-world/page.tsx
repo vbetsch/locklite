60775ed39fdfcb67422d7c8dc6da1d77500460bb
vbetsch <vic.betsch@gmail.com>
2025-07-21 23:11:59 +0200

    feat: Implement API call

--- src/app/hello-world/page.tsx
+++ src/app/hello-world/page.tsx
@@ -1,11 +1,24 @@
 'use client'
 import styles from './page.module.css'
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
 import Title from "@ui/components/common/atoms/Title";
 import UsersList from "@ui/components/users/molecules/UsersList";
+import {RequestService} from "@ui/services/requestService";
+import {UserDto} from "@shared/dto/user.dto";
 
 export default function HelloWorldPage() {
-    const [users, ] = useState<string[]>(['example', 'foo', 'nar'])
+    const [users, setUsers] = useState<string[]>([])
+
+    useEffect(() => {
+        void (async () => {
+            try {
+                const data: UserDto[] = await RequestService.get<UserDto[]>('/api/users')
+                setUsers(data.map(user => user.email))
+            } catch (error) {
+                console.error('Error:', error)
+            }
+        })()
+    }, [])
 
     return (
         <div className={styles.container}>
