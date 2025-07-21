7b9418be3a6ee9a589e8af47c233819c7b41359d
vbetsch <vic.betsch@gmail.com>
2025-07-21 23:35:48 +0200

    refactor: Use UserDto in backend

--- src/modules/api/services/userService.ts
+++ src/modules/api/services/userService.ts
@@ -1,8 +1,15 @@
 import prisma from "@lib/prisma";
 import {MasterAccount} from '@prisma/generated';
+import {UserDto} from "@shared/dto/user.dto";
 
 export class UserService {
-    public static async getAll(): Promise<MasterAccount[]> {
-        return await prisma.masterAccount.findMany();
+    public static async getAll(): Promise<UserDto[]> {
+        const masterAccounts: MasterAccount[] = await prisma.masterAccount.findMany();
+        return masterAccounts.map((account) => ({
+            id: account.id,
+            email: account.email,
+            password: account.password,
+            createdAt: account.createdAt,
+        }))
     }
 }
