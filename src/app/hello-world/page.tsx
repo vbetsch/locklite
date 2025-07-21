2567086d03451468b84928b0b5f666b672292ee3
vbetsch <vic.betsch@gmail.com>
2025-07-21 23:04:37 +0200

    fix: Rewrite paths

--- src/app/hello-world/page.tsx
+++ src/app/hello-world/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 import styles from './page.module.css'
 import { useState } from 'react';
-import Title from "@/components/common/atoms/Title";
-import UsersList from "@/components/users/molecules/UsersList";
+import Title from "@ui/components/common/atoms/Title";
+import UsersList from "@ui/components/users/molecules/UsersList";
 
 export default function HelloWorldPage() {
     const [users, ] = useState<string[]>(['example', 'foo', 'nar'])
