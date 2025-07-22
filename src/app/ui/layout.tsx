bc8350740302148045912bdac77cb4d6cdef2c8c
vbetsch <vic.betsch@gmail.com>
2025-07-22 23:12:54 +0200

    Revert "refactor: Clean the main layout"
    This reverts commit ebfbe64cd61dfc521698b71bc1d47d9468852234.

--- src/app/ui/layout.tsx
+++ src/app/ui/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from "next";
-// import { Geist } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import React from "react";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,17 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/*<body className={`${geistSans.variable} ${geistMono.variable}`}>*/}
-      <body>
-        <header>
-          header
-        </header>
-        <main>
-          {children}
-        </main>
-        <footer>
-          footer
-        </footer>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
       </body>
     </html>
   );
