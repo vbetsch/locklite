ebfbe64cd61dfc521698b71bc1d47d9468852234
vbetsch <vic.betsch@gmail.com>
2025-07-22 23:10:43 +0200

    refactor: Clean the main layout

--- src/app/ui/layout.tsx
+++ src/app/ui/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist } from "next/font/google";
 import "./globals.css";
 import React from "react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,8 +20,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      {/*<body className={`${geistSans.variable} ${geistMono.variable}`}>*/}
+      <body>
+        <header>
+          header
+        </header>
+        <main>
+          {children}
+        </main>
+        <footer>
+          footer
+        </footer>
       </body>
     </html>
   );
