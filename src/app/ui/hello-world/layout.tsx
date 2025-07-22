ed95c3ce783225bb4a43e76b6f1b262e37147d4d
vbetsch <71458556+vbetsch@users.noreply.github.com>
2025-07-22 23:53:18 +0200

    feat: Implement new main layout (#23)
    * fix: Reinstall next-swagger-doc and swagger-jsdoc
    
    * fix: Move UI layout into ui folder
    
    * fix: Add a default API layout
    generated by Next
    
    * refactor: Add react import
    
    * refactor: Clean the main layout
    
    * feat: Add favicon
    
    * feat: Rewrite metadata

---
+++ src/app/ui/hello-world/layout.tsx
@@ -0,0 +1,14 @@
+import type {Metadata} from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+    title: 'Hello World',
+}
+
+export default function HelloWorldLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <>{children}</>
+}
---
+++ src/app/ui/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+// import { Geist } from "next/font/google";
+import "./globals.css";
+import React from "react";
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+export const metadata: Metadata = {
+  title: {
+    default: 'LockLite',
+    template: '%s | LockLite'
+  },
+  description: "The best secure password manager",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      {/*<body className={`${geistSans.variable} ${geistMono.variable}`}>*/}
+      <body>
+        <header>
+          header
+        </header>
+        <main>
+          {children}
+        </main>
+        <footer>
+          footer
+        </footer>
+      </body>
+    </html>
+  );
+}
