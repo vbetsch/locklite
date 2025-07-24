825f349c76040158ca7690127a8b3739122fd4e6
vbetsch <vic.betsch@gmail.com>
2025-07-24 22:50:50 +0200

    chore(tests): Fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,18 +3,18 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        useESM: false,
+      },
+    ],
+  },
   roots: ['<rootDir>'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
-  },
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-    },
   },
 };
 
