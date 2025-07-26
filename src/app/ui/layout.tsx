44522a1a4857a5465a60eb0454b336cdced3a7e6
vbetsch <vic.betsch@gmail.com>
2025-07-26 20:14:24 +0200

    fix: Add html and body in main layout

--- src/app/ui/layout.tsx
+++ src/app/ui/layout.tsx
@@ -19,29 +19,37 @@ export default function RootLayout({
   children: React.ReactNode;
 }): JSX.Element {
   return (
-    <>
-      <ThemeRegistry>
-        <Box className="foobarbaz">
-          <AppBar position="static" component="header">
-            <Toolbar>
-              <Typography variant="h6">My Application</Typography>
-            </Toolbar>
-          </AppBar>
-          <Container component="main" maxWidth="md" sx={{ flexGrow: 1, py: 4 }}>
-            {children}
-          </Container>
-          <Box
-            component="footer"
-            sx={{
-              py: 2,
-              textAlign: 'center',
-              bgcolor: 'grey.100',
-            }}
-          >
-            © {new Date().getFullYear()} MonSite
+    // eslint-disable-next-line no-restricted-syntax
+    <html lang="en">
+      {/* eslint-disable-next-line no-restricted-syntax */}
+      <body>
+        <ThemeRegistry>
+          <Box className="foobarbaz">
+            <AppBar position="static" component="header">
+              <Toolbar>
+                <Typography variant="h6">Locklite</Typography>
+              </Toolbar>
+            </AppBar>
+            <Container
+              component="main"
+              maxWidth="md"
+              sx={{ flexGrow: 1, py: 4 }}
+            >
+              {children}
+            </Container>
+            <Box
+              component="footer"
+              sx={{
+                py: 2,
+                textAlign: 'center',
+                bgcolor: 'grey.100',
+              }}
+            >
+              © {new Date().getFullYear()} Locklite
+            </Box>
           </Box>
-        </Box>
-      </ThemeRegistry>
-    </>
+        </ThemeRegistry>
+      </body>
+    </html>
   );
 }
