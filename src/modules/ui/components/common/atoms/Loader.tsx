9abdf731dc30d32e0ecc44a69e72d686b1c96fc5
vbetsch <vic.betsch@gmail.com>
2025-07-26 19:36:51 +0200

    feat: Create loader and error components

--- src/modules/ui/components/common/atoms/Loader.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import styles from './Loader.module.css';
-import type { JSX } from 'react';
-
-export default function Loader(): JSX.Element {
-  return <span className={styles.loader} aria-label="Loading" />;
-}
