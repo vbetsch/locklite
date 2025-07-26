f54f187289daf1cedb2a2562469dafa74a4c2c6b
vbetsch <vic.betsch@gmail.com>
2025-07-26 17:51:30 +0200

    feat: Add theming

--- src/app/ui/layout.tsx
+++ src/app/ui/layout.tsx
@@ -1,14 +1,8 @@
 import type { Metadata } from 'next';
-// import { Geist } from "next/font/google";
 import './globals.css';
 import type { JSX } from 'react';
 import React from 'react';
 import MUIProvider from '@ui/providers/MUIProvider';
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
 
 export const metadata: Metadata = {
   title: {
@@ -25,7 +19,6 @@ export default function RootLayout({
 }>): JSX.Element {
   return (
     <html lang="en">
-      {/*<body className={`${geistSans.variable} ${geistMono.variable}`}>*/}
       <body>
         <MUIProvider>
           <div className="root">
