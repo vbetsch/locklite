8785c8deb5eed3fc929d3427e0371f8865e467c4
vbetsch <vic.betsch@gmail.com>
2025-07-28 23:01:37 +0200

    test: Create tests of HttpError

---
+++ tests/units/modules/api/errors/abstract/http.test.ts
@@ -0,0 +1,32 @@
+import { HttpError } from '@api/errors/abstract/http-error';
+
+class TestError extends HttpError {
+  public constructor(message: string, status: number) {
+    super(message, status);
+  }
+}
+
+describe('HttpError', () => {
+  it('should correctly set the message, status and name', () => {
+    const message: string = 'An error occurred';
+    const status: number = 418;
+    const error: HttpError = new TestError(message, status);
+
+    expect(error.message).toBe(message);
+    expect(error.status).toBe(status);
+    expect(error.name).toBe('TestError');
+  });
+
+  it('should be an instance of Error', () => {
+    const error: HttpError = new TestError('Error', 500);
+    expect(error).toBeInstanceOf(Error);
+  });
+
+  it('the stack property should contain the class name and the message', () => {
+    const message: string = 'Stack test';
+    const status: number = 400;
+    const error: HttpError = new TestError(message, status);
+    expect(error.stack).toContain('TestError');
+    expect(error.stack).toContain(message);
+  });
+});
