993b38ec2c6d55e442fb01d6f961f1a4da1c51e4
vbetsch <vic.betsch@gmail.com>
2025-07-28 23:48:31 +0200

    test: Create tests for PageContainer

---
+++ tests/units/modules/ui/components/common/PageContainer.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import PageContainer from '@ui/components/common/PageContainer';
+
+describe('PageContainer', () => {
+  it('renders its children', (): void => {
+    render(
+      <PageContainer>
+        {/* eslint-disable-next-line no-restricted-syntax */}
+        <div>Test Content</div>
+      </PageContainer>
+    );
+    expect(screen.getByText('Test Content')).toBeInTheDocument();
+  });
+
+  it('always renders a Container element', (): void => {
+    const { container } = render(
+      <PageContainer>
+        {/* eslint-disable-next-line no-restricted-syntax */}
+        <span>Child</span>
+      </PageContainer>
+    );
+    // MUI Container renders as a div by default
+    const div: HTMLDivElement | null = container.querySelector('div');
+    expect(div).not.toBeNull();
+    expect(screen.getByText('Child')).toBeInTheDocument();
+  });
+});
