eb581502b2fabcca201d0c3984bb9cecde9752e9
vbetsch <vic.betsch@gmail.com>
2025-07-28 22:54:07 +0200

    refactor: Clean

--- src/app/ui/page.tsx
+++ src/app/ui/page.tsx
@@ -1,18 +1,6 @@
 import React from 'react';
 import type { JSX } from 'react';
-import { Typography } from '@mui/material';
-import Image from 'next/image';
 
 export default function Home(): JSX.Element {
-  return (
-    <>
-      <Typography>homepage</Typography>
-      <Image
-        src={'/img/typescript.png'}
-        alt={'example'}
-        height={200}
-        width={200}
-      />
-    </>
-  );
+  return <></>;
 }
--- src/modules/api/errors/users-not-found.error.ts
+++
@@ -1,8 +0,0 @@
-import { HttpError } from '@api/errors/http-error';
-import { StatusCodes } from 'http-status-codes';
-
-export class UsersNotFoundError extends HttpError {
-  public constructor() {
-    super('Users not found', StatusCodes.NOT_FOUND);
-  }
-}
