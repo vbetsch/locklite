bf177530f6a51c00f77f18a80e82a06ca02018e7
vbetsch <vic.betsch@gmail.com>
2025-07-29 19:54:15 +0200

    refactor: Create IAdapter

--- src/modules/api/adapters/vault.adapter.ts
+++ src/modules/api/adapters/vault.adapter.ts
@@ -1,9 +1,10 @@
 import { injectable } from 'tsyringe';
 import { VaultModelDto } from '@shared/dto/models/vault.model.dto';
 import { Vault } from '@prisma/generated';
+import { IAdapter } from '@api/adapters/abstract/adapter.interface';
 
 @injectable()
-export class VaultAdapter {
+export class VaultAdapter implements IAdapter<Vault, VaultModelDto> {
   public getDtoFromModel(model: Vault): VaultModelDto {
     return {
       ...model,
