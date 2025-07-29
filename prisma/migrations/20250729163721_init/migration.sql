b2251fd483d0ea6c66de59039b6a28b7ee74be2e
vbetsch <vic.betsch@gmail.com>
2025-07-29 18:38:18 +0200

    refactor: Add @db.VarChar(255)

--- prisma/migrations/20250729163721_init/migration.sql
+++ prisma/migrations/20250729163721_init/migration.sql
@@ -1,8 +1,8 @@
 -- CreateTable
 CREATE TABLE "Vault" (
     "id" TEXT NOT NULL,
-    "label" TEXT NOT NULL,
-    "secret" TEXT NOT NULL,
+    "label" VARCHAR(255) NOT NULL,
+    "secret" VARCHAR(255) NOT NULL,
 
     CONSTRAINT "Vault_pkey" PRIMARY KEY ("id")
 );
