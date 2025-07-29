120014b6af459393387e7171be44735538ecc4ea
vbetsch <vic.betsch@gmail.com>
2025-07-29 19:48:38 +0200

    feat: Implement CreateVaultUseCase

--- src/modules/api/usecases/vaults/create-vault.usecase.ts
+++ src/modules/api/usecases/vaults/create-vault.usecase.ts
@@ -1,11 +1,22 @@
-import { injectable } from 'tsyringe';
+import { inject, injectable } from 'tsyringe';
 import type { VaultModelDto } from '@shared/dto/models/vault.model.dto';
 import { IUseCaseWithInput } from '@api/usecases/abstract/usecase.with-input.interface';
 import type { CreateVaultParamsDto } from '@shared/dto/params/create-vault.params.dto';
+import { Vault } from '@prisma/generated';
 
 @injectable()
 export class CreateVaultUseCase
   implements IUseCaseWithInput<CreateVaultParamsDto, VaultModelDto>
 {
-  public async handle(params: CreateVaultParamsDto): Promise<VaultModelDto> {}
+  public constructor(
+    @inject(VaultsRepository)
+    private readonly _vaultsRepository: VaultsRepository,
+    @inject(VaultAdapter)
+    private readonly _vaultAdapter: VaultAdapter
+  ) {}
+
+  public async handle(params: CreateVaultParamsDto): Promise<VaultModelDto> {
+    const vaultCreated: Vault = await this._vaultsRepository.create(params);
+    return this._vaultAdapter.getDtoFromModel(vaultCreated);
+  }
 }
