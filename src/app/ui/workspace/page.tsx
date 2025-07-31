272b0c07757db54ae8e068012e2d0a50bf942d7e
vbetsch <vic.betsch@gmail.com>
2025-07-31 23:12:25 +0200

    fix: scrollable list

--- src/app/ui/workspace/page.tsx
+++ src/app/ui/workspace/page.tsx
@@ -33,7 +33,14 @@ export default function WorkspacePage(): JSX.Element {
   });
 
   return (
-    <Container sx={{ padding: '2rem' }}>
+    <Container
+      sx={{
+        padding: '2rem',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '3rem',
+      }}
+    >
       <Typography variant={'h3'} textAlign={'left'}>
         My vaults
       </Typography>
@@ -47,7 +54,8 @@ export default function WorkspacePage(): JSX.Element {
           container
           spacing={{ xs: 2, md: 3, lg: 3, xl: 4 }}
           columns={{ xs: 1, md: 2, lg: 3, xl: 3 }}
-          paddingTop={{ xs: 3, md: 4, lg: 6, xl: 8 }}
+          overflow={'auto'}
+          height={'60vh'}
         >
           {vaults.map(vault => (
             <Grid key={vault.id} size={1}>
