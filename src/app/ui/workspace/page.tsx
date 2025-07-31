f28ab4ef8a2af991d66379fb625e00333688f8fb
vbetsch <vic.betsch@gmail.com>
2025-07-31 21:52:39 +0200

    feat: Implement WorkspacePage

--- src/app/ui/workspace/page.tsx
+++ src/app/ui/workspace/page.tsx
@@ -1,6 +1,48 @@
-import React from 'react';
+'use client';
+
+import 'reflect-metadata';
+import React, { useState } from 'react';
 import type { JSX } from 'react';
+import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import type { VaultModelDto } from '@shared/dto/models/vault.model.dto';
+import ErrorMessage from '@ui/components/common/ErrorMessage';
+import CircularLoader from '@ui/components/common/CircularLoader';
+import { VaultsGateway } from '@ui/gateways/vaults.gateway';
+import { container } from 'tsyringe';
+import type { GetMyVaultsResponseDto } from '@shared/dto/responses/get-my-vaults.response.dto';
+import { useApi } from '@ui/hooks/useApi';
 
 export default function WorkspacePage(): JSX.Element {
-  return <>workspace</>;
+  const [vaults, setVaults] = useState<VaultModelDto[]>([]);
+  const [error, setError] = useState<Error | null>(null);
+  const vaultsGateway: VaultsGateway = container.resolve(VaultsGateway);
+
+  const { loading } = useApi<GetMyVaultsResponseDto>({
+    request: () => vaultsGateway.getMyVaults(),
+    onSuccess: (data) => setVaults(data.myVaults),
+    onError: (err) => setError(err),
+    deps: [],
+  });
+
+  return (
+    <>
+      <ErrorMessage error={error} />
+      <CircularLoader loading={loading} />
+      <List>
+        {vaults.length > 0 ? (
+          vaults.map((item, index) => (
+            <ListItem key={index} disablePadding>
+              <ListItemButton>
+                <ListItemText content={item.label} />
+              </ListItemButton>
+            </ListItem>
+          ))
+        ) : (
+          <ListItem>
+            <ListItemText primary="No results found" />
+          </ListItem>
+        )}
+      </List>
+    </>
+  );
 }
