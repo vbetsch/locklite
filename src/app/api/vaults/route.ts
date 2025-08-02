eda8bafc734931c62f5d1ca637741448bf260d95
vbetsch <vic.betsch@gmail.com>
2025-08-02 20:34:25 +0200

    fix: api typing

--- src/app/api/vaults/route.ts
+++ src/app/api/vaults/route.ts
@@ -3,12 +3,13 @@ import type { NextRequest, NextResponse } from 'next/server';
 import { container } from 'tsyringe';
 import { handleApiRequest } from '@api/helpers/handle-api-request';
 import type { VaultModelDto } from '@shared/dto/models/vault.model.dto';
-import type { CreateVaultResponseDto } from '@shared/dto/responses/create-vault.response.dto';
 import { CreateVaultUseCase } from '@api/usecases/vaults/create-vault.usecase';
-import type { GetMyVaultsResponseDto } from '@shared/dto/responses/get-my-vaults.response.dto';
 import { GetMyVaultsUseCase } from '@api/usecases/vaults/get-my-vaults.usecase';
 import type { CreateVaultRequestDto } from '@shared/dto/requests/create-vault.request.dto';
 import { StatusCodes } from 'http-status-codes';
+import type { CreateVaultDataDto } from '@shared/dto/data/create-vault.data.dto';
+import type { GetMyVaultsDataDto } from '@shared/dto/data/get-my-vaults.data.dto';
+import type { HttpResponseDto } from '@shared/dto/responses/abstract/http.response.dto';
 
 /**
  * @swagger
@@ -31,12 +32,14 @@ import { StatusCodes } from 'http-status-codes';
  *             schema:
  *               $ref: '#/components/schemas/HttpResponseDto'
  */
-export async function GET(): Promise<NextResponse> {
+export async function GET(): Promise<
+  NextResponse<HttpResponseDto<GetMyVaultsDataDto>>
+> {
   const getMyVaultsUseCase: GetMyVaultsUseCase =
     container.resolve(GetMyVaultsUseCase);
-  return await handleApiRequest<GetMyVaultsResponseDto>(async () => {
+  return await handleApiRequest<GetMyVaultsDataDto>(async () => {
     const myVaults: VaultModelDto[] = await getMyVaultsUseCase.handle();
-    const response: GetMyVaultsResponseDto = { data: { myVaults } };
+    const response: GetMyVaultsDataDto = { myVaults };
     return response;
   });
 }
@@ -60,7 +63,7 @@ export async function GET(): Promise<NextResponse> {
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/CreateVaultResponseDto'
+ *               $ref: '#/components/schemas/CreateVaultDataDto'
  *       400:
  *         description: One of the requested values is too long
  *         content:
@@ -74,13 +77,15 @@ export async function GET(): Promise<NextResponse> {
  *             schema:
  *               $ref: '#/components/schemas/HttpResponseDto'
  */
-export async function POST(request: NextRequest): Promise<NextResponse> {
+export async function POST(
+  request: NextRequest
+): Promise<NextResponse<HttpResponseDto<CreateVaultDataDto>>> {
   const params: CreateVaultRequestDto = await request.json();
   const createVaultUseCase: CreateVaultUseCase =
     container.resolve(CreateVaultUseCase);
-  return await handleApiRequest<CreateVaultResponseDto>(async () => {
+  return await handleApiRequest<CreateVaultDataDto>(async () => {
     const vaultCreated: VaultModelDto = await createVaultUseCase.handle(params);
-    const response: CreateVaultResponseDto = { data: { vaultCreated } };
+    const response: CreateVaultDataDto = { vaultCreated };
     return response;
   }, StatusCodes.CREATED);
 }
--- src/modules/api/helpers/handle-api-request.ts
+++ src/modules/api/helpers/handle-api-request.ts
@@ -1,21 +1,25 @@
 import { HttpError } from '@api/errors/abstract/http-error';
 import { StatusCodes } from 'http-status-codes';
 import { NextResponse } from 'next/server';
+import type { HttpResponseDto } from '@shared/dto/responses/abstract/http.response.dto';
 
-export async function handleApiRequest<T>(
-  callback: () => Promise<T>,
+export async function handleApiRequest<Data>(
+  callback: () => Promise<Data>,
   successStatusCode?: StatusCodes
-): Promise<NextResponse> {
+): Promise<NextResponse<HttpResponseDto<Data>>> {
   try {
-    const data: Awaited<T> = await callback();
+    const data: Awaited<Data> = await callback();
 
     if (successStatusCode === StatusCodes.NO_CONTENT) {
       return new NextResponse(null, { status: StatusCodes.NO_CONTENT });
     }
 
-    return NextResponse.json(data, {
-      status: successStatusCode || StatusCodes.OK,
-    });
+    return NextResponse.json(
+      { data },
+      {
+        status: successStatusCode || StatusCodes.OK,
+      }
+    );
   } catch (error: unknown) {
     if (error instanceof HttpError) {
       return NextResponse.json(
