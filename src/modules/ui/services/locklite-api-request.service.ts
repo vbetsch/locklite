3f705880da069171b7a891c51c428237938b48c7
vbetsch <vic.betsch@gmail.com>
2025-08-03 20:51:40 +0200

    fix: url in LockliteApiRequestService

--- src/modules/ui/services/locklite-api-request.service.ts
+++ src/modules/ui/services/locklite-api-request.service.ts
@@ -9,6 +9,8 @@ import { BusinessError } from '@shared/errors/business-error';
 
 @injectable()
 export class LockliteApiRequestService extends RequestService {
+  private readonly _baseUrl: string = '/api';
+
   private _returnStatusWithoutData<Data>(): RequestServiceOutputType<Data> {
     return {
       status: StatusCodes.NO_CONTENT,
@@ -59,7 +61,10 @@ export class LockliteApiRequestService extends RequestService {
     uri: string,
     options: RequestInit
   ): Promise<RequestServiceOutputType<Data>> {
-    const response: Response = await this._fetch(uri, options);
+    const response: Response = await this._fetch(
+      `${this._baseUrl}${uri}`,
+      options
+    );
 
     if (response.status === StatusCodes.NO_CONTENT) {
       return this._returnStatusWithoutData<Data>();
