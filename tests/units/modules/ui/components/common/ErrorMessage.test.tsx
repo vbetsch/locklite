9cad491b4f559e9c1feb1e73864f5db5489774a7
vbetsch <vic.betsch@gmail.com>
2025-08-03 02:56:11 +0200

    fix: error message tests

--- tests/units/modules/ui/components/common/ErrorMessage.test.tsx
+++ tests/units/modules/ui/components/common/ErrorMessage.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import ErrorMessage from '@ui/components/common/ErrorMessage';
+import { UiLogger } from '@ui/logs/ui.logger';
 
 describe('ErrorMessage', () => {
   it('renders nothing when error is null', (): void => {
@@ -11,37 +12,37 @@ describe('ErrorMessage', () => {
 
   it('renders the error message and logs error when an Error is provided', (): void => {
     const error: Error = new Error('Test error');
-    const consoleErrorSpy: jest.SpyInstance<void, [unknown]> = jest
-      .spyOn(console, 'error')
+    const loggerSpy: jest.SpyInstance<void, [unknown, Error]> = jest
+      .spyOn(UiLogger, 'error')
       .mockImplementation((): void => {
         return;
       });
 
     render(<ErrorMessage error={error} />);
-    expect(consoleErrorSpy).toHaveBeenCalledWith(error);
+    expect(loggerSpy).toHaveBeenCalledWith(null, error);
 
     const messageElement: HTMLElement = screen.getByText('Error: Test error');
     expect(messageElement).toBeInTheDocument();
 
-    consoleErrorSpy.mockRestore();
+    loggerSpy.mockRestore();
   });
 
   it("renders 'Unknown error' when error.message is empty", (): void => {
     const error: Error = new Error('');
-    const consoleErrorSpy: jest.SpyInstance<void, [unknown]> = jest
-      .spyOn(console, 'error')
+    const loggerSpy: jest.SpyInstance<void, [unknown, Error]> = jest
+      .spyOn(UiLogger, 'error')
       .mockImplementation((): void => {
         return;
       });
 
     render(<ErrorMessage error={error} />);
-    expect(consoleErrorSpy).toHaveBeenCalledWith(error);
+    expect(loggerSpy).toHaveBeenCalledWith(null, error);
 
     const messageElement: HTMLElement = screen.getByText(
       'Error: Unknown error'
     );
     expect(messageElement).toBeInTheDocument();
 
-    consoleErrorSpy.mockRestore();
+    loggerSpy.mockRestore();
   });
 });
