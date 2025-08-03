90dc96a313fd04bc548b10a8b5c0c98669110d7d
vbetsch <vic.betsch@gmail.com>
2025-08-03 02:14:36 +0200

    feat: make Logger static

--- src/modules/shared/logs/logger.ts
+++ src/modules/shared/logs/logger.ts
@@ -1,12 +1,11 @@
-import { injectable } from 'tsyringe';
 import { LoggerTagEnum } from '@shared/logs/logger-tag.enum';
 import { LoggerColorEnum } from '@shared/logs/logger-color.enum';
 
-@injectable()
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class Logger {
-  private readonly _prefix: string = '➔';
+  private static readonly _prefix: string = '➔';
 
-  private _compute(
+  private static _compute(
     tag: LoggerTagEnum,
     message: string,
     color?: LoggerColorEnum
@@ -14,52 +13,52 @@ export class Logger {
     if (process.env.NODE_ENV !== 'development') return;
     const content: string = `${this._prefix} ${tag}: ${message}`;
     if (color) {
-      console.log(`${color}${content}${LoggerColorEnum.RESET}`);
+      return `${color}${content}${LoggerColorEnum.RESET}`;
     }
     return content;
   }
 
-  public ok(tag: LoggerTagEnum, message: string): void {
+  public static ok(tag: LoggerTagEnum, message: string): void {
     console.log(
       this._compute(LoggerTagEnum.OK, message, LoggerColorEnum.SUCCESS)
     );
   }
 
-  public done(tag: LoggerTagEnum, message: string): void {
+  public static done(tag: LoggerTagEnum, message: string): void {
     console.log(
       this._compute(LoggerTagEnum.DONE, message, LoggerColorEnum.SUCCESS)
     );
   }
 
-  public log(message: string): void {
+  public static log(message: string): void {
     console.log(this._compute(LoggerTagEnum.LOG, message));
   }
 
-  public debug(message: string): void {
+  public static debug(message: string): void {
     console.debug(
       this._compute(LoggerTagEnum.DEBUG, message, LoggerColorEnum.DEBUG)
     );
   }
 
-  public info(message: string): void {
+  public static info(message: string): void {
     console.info(
       this._compute(LoggerTagEnum.INFO, message, LoggerColorEnum.INFO)
     );
   }
 
-  public warn(message: string): void {
+  public static warn(message: string): void {
     console.warn(
       this._compute(LoggerTagEnum.WARN, message, LoggerColorEnum.WARNING)
     );
   }
 
-  public error(message: string): void {
+  public static error(message: string): void {
     console.error(
       this._compute(LoggerTagEnum.ERROR, message, LoggerColorEnum.ERROR)
     );
   }
 
-  public critical(message: string): void {
+  public static critical(message: string): void {
     console.error(
       this._compute(LoggerTagEnum.CRITICAL, message, LoggerColorEnum.ERROR)
     );
