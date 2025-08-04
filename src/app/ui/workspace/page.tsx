74c3a74e41d7397fd4a79db99dc51b16c3122ec3
vbetsch <vic.betsch@gmail.com>
2025-08-05 01:33:54 +0200

    refactor: create DynamicVaultsList component

--- src/app/ui/workspace/page.tsx
+++ src/app/ui/workspace/page.tsx
@@ -1,26 +1,12 @@
 'use client';
 
 import 'reflect-metadata';
-import React, { useState } from 'react';
+import React from 'react';
 import type { JSX } from 'react';
-import type { VaultModelDto } from '@shared/dto/models/vault.model.dto';
-import ErrorMessage from '@ui/components/common/ErrorMessage';
-import { Box, Button, Container, Typography } from '@mui/material';
-import AddVaultModal from '@ui/components/modals/AddVaultModal';
-import { useVaults } from '@ui/hooks/useVaults';
-import AddIcon from '@mui/icons-material/Add';
-import SearchBar from '@ui/components/common/SearchBar';
-import VaultsList from '@ui/components/vaults/organisms/VaultsList';
+import DynamicVaultsList from '@ui/components/vaults/templates/DynamicVaultsList';
+import { Container, Typography } from '@mui/material';
 
 export default function WorkspacePage(): JSX.Element {
-  const { vaults, loading, error, refetch } = useVaults();
-  const [open, setOpen] = useState(false);
-
-  const [searchTerm, setSearchTerm] = useState('');
-  const filteredVaults: VaultModelDto[] = vaults.filter(v =>
-    v.label.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
   return (
     <Container
       sx={{
@@ -30,38 +16,10 @@ export default function WorkspacePage(): JSX.Element {
         gap: '3rem',
       }}
     >
-      <AddVaultModal
-        open={open}
-        onClose={() => setOpen(false)}
-        refreshVaults={refetch}
-      />
       <Typography variant={'h3'} textAlign={'left'}>
         My vaults
       </Typography>
-      <Box
-        sx={{
-          display: 'flex',
-          gap: '1rem',
-          width: '100%',
-        }}
-      >
-        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <Button
-          startIcon={<AddIcon />}
-          variant="contained"
-          sx={{ minWidth: 150 }}
-          onClick={() => setOpen(true)}
-        >
-          Add a vault
-        </Button>
-      </Box>
-      <ErrorMessage error={error} />
-      <VaultsList
-        loading={loading}
-        searchTerm={searchTerm}
-        displayedVaults={filteredVaults}
-        refetchVaults={refetch}
-      />
+      <DynamicVaultsList />
     </Container>
   );
 }
