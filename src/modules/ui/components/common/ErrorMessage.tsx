3ff44ca8aea639b19dcb8ff08525d44aef59fc98
vbetsch <vic.betsch@gmail.com>
2025-08-04 23:36:13 +0200

    fix: spam error in console

--- src/modules/ui/components/common/ErrorMessage.tsx
+++ src/modules/ui/components/common/ErrorMessage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import type { JSX } from 'react';
 import { Typography } from '@mui/material';
-import { UiLogger } from '@ui/logs/ui.logger';
 
 type ErrorProps = {
   error: Error | null;
@@ -9,7 +8,6 @@ type ErrorProps = {
 
 export default function ErrorMessage(props: ErrorProps): JSX.Element | null {
   if (!props.error) return null;
-  UiLogger.error(null, props.error);
   return (
     <Typography sx={{ color: 'red' }}>
       Error: {props.error.message || 'Unknown error'}
