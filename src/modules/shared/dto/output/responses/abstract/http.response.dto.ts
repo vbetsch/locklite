e918da42a155fc127087e9b793494d74860a4bad
vbetsch <vic.betsch@gmail.com>
2025-08-06 01:28:59 +0200

    fix: Implement BusinessErrorDto in HttpResponseDto

--- src/modules/shared/dto/output/responses/abstract/http.response.dto.ts
+++ src/modules/shared/dto/output/responses/abstract/http.response.dto.ts
@@ -1,4 +1,8 @@
 import type { HttpBodyDto } from '@shared/dto/output/body/abstract/http.body.dto';
 import type { HttpErrorDto } from '@shared/dto/output/errors/http.error.dto';
+import type { BusinessErrorDto } from '@shared/dto/output/errors/business.error.dto';
 
-export type HttpResponseDto<Data> = HttpBodyDto<Data> | HttpErrorDto;
+export type HttpResponseDto<Data> =
+  | HttpBodyDto<Data>
+  | HttpErrorDto
+  | BusinessErrorDto;
