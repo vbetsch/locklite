5a64e89c603f87962144d438a733de7c64340100
vbetsch <vic.betsch@gmail.com>
2025-08-05 19:03:23 +0200

    fix: add auth layout

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,22 @@
+import type { JSX } from 'react';
+import React from 'react';
+import type { Metadata } from 'next';
+import type { SharedLayoutProps } from '@shared/props/SharedLayoutProps';
+
+export const metadata: Metadata = {
+  title: 'LockLite Auth',
+  description:
+    'The authentification management of the Locklite password manager',
+};
+
+export default function RootLayout({
+  children,
+}: SharedLayoutProps): JSX.Element {
+  return (
+    // eslint-disable-next-line no-restricted-syntax
+    <html lang="en">
+      {/* eslint-disable-next-line no-restricted-syntax */}
+      <body>{children}</body>
+    </html>
+  );
+}
