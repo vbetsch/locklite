4e2967fa47328a78edbf2f43fe2f7a94012f1fde
vbetsch <vic.betsch@gmail.com>
2025-08-05 18:58:54 +0200

    feat: add next auth routing

---
+++ src/app/auth/route.ts
@@ -0,0 +1,47 @@
+import type { NextAuthOptions } from 'next-auth';
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from '@lib/prisma';
+import { compare } from 'bcrypt';
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials) return null;
+        const user: {
+          name: string | null;
+          id: string;
+          email: string;
+          password: string;
+        } | null = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+        if (!user) return null;
+        const isValid: boolean = await compare(
+          credentials.password,
+          user.password
+        );
+        if (!isValid) return null;
+        return { id: user.id, email: user.email };
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler: typeof NextAuth = NextAuth(authOptions);
+export { handler as GET, handler as POST };
