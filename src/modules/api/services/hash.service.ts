7559c34d1939062481d55c9eb224fab6872d0dd4
vbetsch <vic.betsch@gmail.com>
2025-08-06 01:11:53 +0200

    feat: implement compare hash

--- src/modules/api/services/hash.service.ts
+++ src/modules/api/services/hash.service.ts
@@ -1,5 +1,5 @@
 import { injectable } from 'tsyringe';
-import { hash } from 'bcrypt';
+import { compare, hash } from 'bcrypt';
 import { ApiLogger } from '@api/logs/api.logger';
 
 @injectable()
@@ -9,15 +9,31 @@ export class HashService {
     10
   );
 
-  private async _hashString(str: string): Promise<string> {
+  private async _hashString(str: string | Buffer): Promise<string> {
     return await hash(str, this._salt);
+  }
+
+  private async _compareTwoHash(str: string, ref: string): Promise<boolean> {
+    return await compare(str, ref);
   }
 
   public async hash(str: string): Promise<string> {
     try {
       return await this._hashString(str);
     } catch (error: unknown) {
-      ApiLogger.error('An error occurred while hashing : ', error);
+      ApiLogger.error('An error occurred while hashing string : ', error);
+      throw error;
+    }
+  }
+
+  public async compare(
+    hashedInput: string,
+    refValue: string
+  ): Promise<boolean> {
+    try {
+      return await this._compareTwoHash(hashedInput, refValue);
+    } catch (error: unknown) {
+      ApiLogger.error('An error occurred while compare two hash : ', error);
       throw error;
     }
   }
