a89f3ece46936c59f7d2f63aaab7855f0cc51874
vbetsch <vic.betsch@gmail.com>
2025-08-07 00:31:22 +0200

    fix: need SessionProvider

--- src/app/ui/workspace/layout.tsx
+++ src/app/ui/workspace/layout.tsx
@@ -1,8 +1,15 @@
+'use client';
+
 import React from 'react';
 import type { SharedLayoutProps } from '@shared/props/SharedLayoutProps';
 import type { JSX } from 'react';
 import AuthGuard from '@ui/guards/AuthGuard';
+import { SessionProvider } from 'next-auth/react';
 
 export default function WorkspaceLayout(props: SharedLayoutProps): JSX.Element {
-  return <AuthGuard>{props.children}</AuthGuard>;
+  return (
+    <SessionProvider>
+      <AuthGuard>{props.children}</AuthGuard>
+    </SessionProvider>
+  );
 }
