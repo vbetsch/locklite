a15f56c66830d196b9e3208343485256a939ca9a
vbetsch <vic.betsch@gmail.com>
2025-08-07 01:17:13 +0200

    feat: implement Next Auth in handleApiRequest

--- src/modules/api/helpers/api/handle-api-request.ts
+++ src/modules/api/helpers/api/handle-api-request.ts
@@ -1,16 +1,40 @@
 import { HttpError } from '@shared/errors/http-error';
 import { StatusCodes } from 'http-status-codes';
+import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import type { HttpResponseDto } from '@shared/dto/output/responses/abstract/http.response.dto';
 import { ApiLogger } from '@api/logs/api.logger';
 import { InternalServerError } from '@api/errors/http/internal-server.error';
 import { BusinessError } from '@shared/errors/business-error';
+import type { JWT } from 'next-auth/jwt';
+import { getToken } from 'next-auth/jwt';
+import { UnauthorizedError } from '@api/errors/http/unauthorized.error';
+import type { Session } from 'next-auth';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@lib/auth';
 
 export async function handleApiRequest<Data>(
+  request: NextRequest,
+  needToBeAuthenticated: boolean,
   callback: () => Promise<Data>,
   successStatusCode?: StatusCodes
 ): Promise<NextResponse<HttpResponseDto<Data>>> {
   try {
+    const token: JWT | null = await getToken({
+      req: request,
+      secret: process.env.NEXTAUTH_SECRET,
+    });
+    if (!token) {
+      throw new UnauthorizedError();
+    }
+
+    if (needToBeAuthenticated) {
+      const session: Session | null = await getServerSession(authOptions);
+      if (!session) {
+        throw new UnauthorizedError();
+      }
+    }
+
     const data: Awaited<Data> = await callback();
 
     if (successStatusCode === StatusCodes.NO_CONTENT) {
