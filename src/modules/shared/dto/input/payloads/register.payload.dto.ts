28df0197fde46fdadb79f915fd3c6c40904dba44
vbetsch <vic.betsch@gmail.com>
2025-08-06 21:53:33 +0200

    feat: add optional name in RegisterPayloadDto

--- src/modules/shared/dto/input/payloads/register.payload.dto.ts
+++ src/modules/shared/dto/input/payloads/register.payload.dto.ts
@@ -4,6 +4,9 @@
  *   schemas:
  *     RegisterPayloadDto:
  *       type: object
+ *       required:
+ *        - email
+ *        - password
  *       properties:
  *         email:
  *           type: string
@@ -11,8 +14,11 @@
  *         password:
  *           type: string
  *           format: password
+ *         name:
+ *           type: string
  */
 export type RegisterPayloadDto = {
   email: string;
   password: string;
+  name?: string;
 };
