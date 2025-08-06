d274ffed56119229d9ae2ec98666a2c6f726eb1f
vbetsch <vic.betsch@gmail.com>
2025-08-06 22:43:51 +0200

    feat: add menu profile in NavBar

--- src/modules/ui/components/navigation/molecules/NavBar.tsx
+++ src/modules/ui/components/navigation/molecules/NavBar.tsx
@@ -1,16 +1,67 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import type { JSX } from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
 type NavBarProps = {
   title: string;
 };
 
 export default function NavBar(props: NavBarProps): JSX.Element {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>): void => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = (): void => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position={'sticky'} component="header">
       <Toolbar>
         <Typography variant="h6">{props.title}</Typography>
+        <Box>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+          </Menu>
+        </Box>
       </Toolbar>
     </AppBar>
   );
