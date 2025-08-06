26e4fe157706ae886998af441e757f4c5e3b3d29
vbetsch <vic.betsch@gmail.com>
2025-08-07 00:34:18 +0200

    refactor: use SessionStatus

--- src/modules/ui/guards/AuthGuard.tsx
+++ src/modules/ui/guards/AuthGuard.tsx
@@ -5,6 +5,7 @@ import { useSession, signIn } from 'next-auth/react';
 import { Box, CircularProgress } from '@mui/material';
 import type { JSX } from 'react';
 import type { SharedChildrenProps } from '@shared/props/SharedChildrenProps';
+import { SessionStatus } from '@shared/auth/session-status.enum';
 
 export default function AuthGuard(props: SharedChildrenProps): JSX.Element {
   const { status } = useSession({
@@ -13,7 +14,7 @@ export default function AuthGuard(props: SharedChildrenProps): JSX.Element {
       await signIn();
     },
   });
-  if (status === 'loading') {
+  if (status === SessionStatus.LOADING) {
     return (
       <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
         <CircularProgress />
