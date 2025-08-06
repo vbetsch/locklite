ca8fb3d0b8d7c6d833e5466daa85267178a0b69e
vbetsch <vic.betsch@gmail.com>
2025-08-06 23:32:11 +0200

    refactor: move SessionProvider in ClientProviders

--- src/modules/ui/providers/ClientProviders.tsx
+++ src/modules/ui/providers/ClientProviders.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import ThemeRegistry from '@ui/providers/ThemeRegistry';
 import MainNavBar from '@ui/components/navigation/organisms/MainNavBar';
 import { Container } from '@mui/material';
+import { SessionProvider } from 'next-auth/react';
 
 type ClientProvidersProps = {
   children: React.ReactNode;
@@ -15,7 +16,9 @@ export default function ClientProviders({
 }: ClientProvidersProps): JSX.Element {
   return (
     <ThemeRegistry>
-      <MainNavBar />
+      <SessionProvider>
+        <MainNavBar />
+      </SessionProvider>
       <Container component="main">{children}</Container>
     </ThemeRegistry>
   );
