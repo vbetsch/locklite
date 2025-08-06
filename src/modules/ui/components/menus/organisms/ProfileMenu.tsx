a6791840e7a7e5444e5b8f5f8a3351734ff78f7a
vbetsch <vic.betsch@gmail.com>
2025-08-07 00:00:07 +0200

    refactor: remove redirect to login after logout

--- src/modules/ui/components/menus/organisms/ProfileMenu.tsx
+++ src/modules/ui/components/menus/organisms/ProfileMenu.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import type { JSX } from 'react';
 import { Divider, Menu, MenuItem, Typography } from '@mui/material';
 import { signOut, useSession } from 'next-auth/react';
-import { redirect } from 'next/navigation';
-import { RoutesEnum } from '@ui/router/routes.enum';
 
 type ProfileMenuProps = {
   anchorEl: HTMLElement | null;
@@ -24,7 +22,6 @@ export default function ProfileMenu(
   const handleLogout = async (): Promise<void> => {
     await signOut();
     handleClose();
-    redirect(RoutesEnum.LOGIN);
   };
 
   return (
