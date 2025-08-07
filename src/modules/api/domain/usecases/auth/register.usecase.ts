929dca50055c6256762d36ae8d2deba7e7fe5314
vbetsch <vic.betsch@gmail.com>
2025-08-08 01:49:52 +0200

    refactor: Rewrite RegisterUseCase

--- src/modules/api/domain/usecases/auth/register.usecase.ts
+++ src/modules/api/domain/usecases/auth/register.usecase.ts
@@ -21,21 +21,28 @@ export class RegisterUseCase
     private readonly _userAdapter: UserAdapter
   ) {}
 
-  public async handle(input: RegisterPayloadDto): Promise<UserModelDto> {
+  private async _testIfUserExists(email: string): Promise<void | never> {
     const userFound: User | null = await this._usersRepository.findByEmail({
-      email: input.email,
+      email: email,
     });
     if (userFound) {
-      throw new UserAlreadyExistsError(input.email);
+      throw new UserAlreadyExistsError(email);
     }
+  }
 
+  private async _hashPasswordAndCreateUser(
+    input: RegisterPayloadDto
+  ): Promise<User> {
     const inputHashed: RegisterPayloadDto = {
       ...input,
       password: await this._hashService.hash(input.password),
     };
+    return await this._usersRepository.create({ ...inputHashed });
+  }
 
-    const user: User = await this._usersRepository.create({ ...inputHashed });
-
+  public async handle(input: RegisterPayloadDto): Promise<UserModelDto> {
+    await this._testIfUserExists(input.email);
+    const user: User = await this._hashPasswordAndCreateUser(input);
     return this._userAdapter.getDtoFromEntity(user);
   }
 }
