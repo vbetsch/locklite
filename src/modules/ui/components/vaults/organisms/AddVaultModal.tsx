cd1027b91d0a6370eedac92255986698b68ee420
vbetsch <vic.betsch@gmail.com>
2025-08-08 00:46:43 +0200

    docs: add TODO comment

--- src/modules/ui/components/vaults/organisms/AddVaultModal.tsx
+++ src/modules/ui/components/vaults/organisms/AddVaultModal.tsx
@@ -67,6 +67,7 @@ export default function AddVaultModal(props: AddVaultModalProps): JSX.Element {
 
   const handleSubmit = async (): Promise<void> => {
     setGlobalError(null);
+    // TODO: remove userId
     await createVault({ label: newLabel, secret: newSecret, userId: '' });
   };
 
