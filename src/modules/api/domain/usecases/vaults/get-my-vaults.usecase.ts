a4515ffa6ee655ebb8b81b7234a03b868a900fff
vbetsch <vic.betsch@gmail.com>
2025-08-08 02:27:34 +0200

    refactor: create CurrentUserService

--- src/modules/api/domain/usecases/vaults/get-my-vaults.usecase.ts
+++ src/modules/api/domain/usecases/vaults/get-my-vaults.usecase.ts
@@ -4,41 +4,21 @@ import { inject, injectable } from 'tsyringe';
 import { VaultsRepository } from '@api/infra/repositories/vaults.repository';
 import { VaultAdapter } from '@api/app/adapters/vault.adapter';
 import { Vault, User as UserModel } from '@prisma/generated';
-import { getServerSession, Session } from 'next-auth';
-import { authOptions } from '@lib/auth';
-import { UnauthorizedError } from '@api/app/errors/unauthorized.error';
-import { ImpossibleCaseError } from '@api/domain/errors/impossible-case.error';
-import { UsersRepository } from '@api/infra/repositories/users.repository';
+import { CurrentUserService } from '@api/domain/services/current-user.service';
 
 @injectable()
 export class GetMyVaultsUseCase implements IUseCase<VaultModelDto[]> {
   public constructor(
     @inject(VaultsRepository)
     private readonly _vaultsRepository: VaultsRepository,
-    @inject(UsersRepository)
-    private readonly _usersRepository: UsersRepository,
+    @inject(CurrentUserService)
+    private readonly _currentUserService: CurrentUserService,
     @inject(VaultAdapter)
     private readonly _vaultAdapter: VaultAdapter
   ) {}
 
-  private async _getCurrentUser(): Promise<UserModel> {
-    const session: Session | null = await getServerSession(authOptions);
-    if (!session || !session.user || !session.user.email) {
-      throw new UnauthorizedError();
-    }
-    const userFound: UserModel | null = await this._usersRepository.findByEmail(
-      {
-        email: session.user.email,
-      }
-    );
-    if (!userFound) {
-      throw new ImpossibleCaseError();
-    }
-    return userFound;
-  }
-
   public async handle(): Promise<VaultModelDto[]> {
-    const currentUser: UserModel = await this._getCurrentUser();
+    const currentUser: UserModel = await this._currentUserService.get();
     const myVaults: Vault[] = await this._vaultsRepository.findByUserId({
       userId: currentUser.id,
     });
