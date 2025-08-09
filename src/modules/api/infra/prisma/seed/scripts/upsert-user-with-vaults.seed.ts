740d9b6957aef65279ba5307fd8ec6cb8613b864
vbetsch <vic.betsch@gmail.com>
2025-08-09 17:07:34 +0200

    refactor: use HashService

--- src/modules/api/infra/prisma/seed/scripts/upsert-user-with-vaults.seed.ts
+++ src/modules/api/infra/prisma/seed/scripts/upsert-user-with-vaults.seed.ts
@@ -1,15 +1,15 @@
+import 'reflect-metadata';
 import type { UserTypeSeed } from '@api/infra/prisma/seed/types/user.type.seed';
-import bcrypt from 'bcrypt';
 import type { VaultTypeSeed } from '@api/infra/prisma/seed/types/vault.type.seed';
 import type { User, Vault } from '@prisma/generated';
 import prisma from '@lib/prisma';
+import { HashService } from '@api/domain/services/hash.service';
+import { container } from 'tsyringe';
+
+const hashService: HashService = container.resolve(HashService);
 
 export async function upsertUserWithVaults(seed: UserTypeSeed): Promise<User> {
-  const saltRounds: number = 12;
-  const passwordHash: string = await bcrypt.hash(
-    seed.passwordPlain,
-    saltRounds
-  );
+  const passwordHash: string = await hashService.hash(seed.passwordPlain);
 
   const user: User = await prisma.user.upsert({
     where: { email: seed.email },
