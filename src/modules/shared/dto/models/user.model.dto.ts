05555f8af86f1d6991c73e98aab62ef616509489
vbetsch <vic.betsch@gmail.com>
2025-08-10 00:03:46 +0200

    docs: add descriptions

--- src/modules/shared/dto/models/user.model.dto.ts
+++ src/modules/shared/dto/models/user.model.dto.ts
@@ -10,11 +10,14 @@
  *       properties:
  *         id:
  *           type: string
+ *           description: UUID
  *         email:
  *           type: string
+ *           description: user's email
  *           format: email
  *         name:
  *           type: string
+ *           description: fist name and last name
  */
 export type UserModelDto = {
   id: string;
--- src/modules/shared/dto/models/vault.model.dto.ts
+++ src/modules/shared/dto/models/vault.model.dto.ts
@@ -7,11 +7,15 @@
  *       properties:
  *         id:
  *           type: string
+ *           description: UUID
  *           format: uuid
  *         label:
  *           type: string
+ *           description: name of the vault
+ *           example: Google, Amazon, Netflix, Disney+
  *         secret:
  *           type: string
+ *           description: password, code, token, any string
  *           format: password
  */
 export type VaultModelDto = {
