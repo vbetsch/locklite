0fadda8ad2ef75f0a604c48123b42a485a4c8e8b
vbetsch <vic.betsch@gmail.com>
2025-08-12 01:10:47 +0200

    feat: improve VaultCardMembers

--- src/projects/ui/modules/vaults/components/atoms/VaultCardMembers.tsx
+++ src/projects/ui/modules/vaults/components/atoms/VaultCardMembers.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { JSX } from 'react';
-import { AvatarGroup } from '@mui/material';
+import { AvatarGroup, Box, Typography } from '@mui/material';
 import ColorfulLetterAvatar from '@ui/components/avatars/ColorfulLetterAvatar';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import type { UserModelDto } from '@shared/dto/models/user.model.dto';
@@ -13,14 +13,26 @@ export default function VaultCardMembers(
   props: VaultCardMembersProps
 ): JSX.Element {
   return (
-    <AvatarGroup max={4}>
-      {props.members.map(member =>
-        member.name ? (
-          <ColorfulLetterAvatar key={member.email} userName={member.name} />
-        ) : (
-          <AccountCircle key={member.email} />
-        )
-      )}
-    </AvatarGroup>
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        gap: '1rem',
+      }}
+    >
+      <Typography variant="body2" color="text.secondary">
+        Members:
+      </Typography>
+      <AvatarGroup max={4}>
+        {props.members.map(member =>
+          member.name ? (
+            <ColorfulLetterAvatar key={member.email} userName={member.name} />
+          ) : (
+            <AccountCircle key={member.email} />
+          )
+        )}
+      </AvatarGroup>
+    </Box>
   );
 }
