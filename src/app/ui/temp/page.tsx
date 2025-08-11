5470e14451c8c0822e25a3ca822347e34d7af634
vbetsch <vic.betsch@gmail.com>
2025-08-11 23:41:58 +0200

    feat: create temp page

---
+++ src/app/ui/temp/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import type { JSX } from 'react';
+import { useUsers } from '@ui/modules/users/hooks/useUsers';
+import CircularLoader from '@ui/components/loaders/CircularLoader';
+import ErrorMessage from '@ui/components/errors/ErrorMessage';
+
+export default function TempPage(): JSX.Element {
+  const { users, loading, error } = useUsers();
+  return (
+    // eslint-disable-next-line no-restricted-syntax
+    <ul>
+      <CircularLoader loading={loading} />
+      <ErrorMessage error={error} />
+      {users.map(user => (
+        // eslint-disable-next-line no-restricted-syntax
+        <li key={user.id}>
+          {/* eslint-disable-next-line no-restricted-syntax */}
+          <span>{user.id}</span>
+          {/* eslint-disable-next-line no-restricted-syntax */}
+          <span>{user.name}</span>
+          {/* eslint-disable-next-line no-restricted-syntax */}
+          <span>{user.email}</span>
+        </li>
+      ))}
+    </ul>
+  );
+}
