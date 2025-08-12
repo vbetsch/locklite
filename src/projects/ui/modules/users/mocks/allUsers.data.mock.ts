4adcbf5c979661e2d5f932f86c3864c5f7dce0c7
vbetsch <vic.betsch@gmail.com>
2025-08-12 20:47:51 +0200

    refactor: create currentUserDataMock

--- src/projects/ui/modules/users/mocks/allUsers.data.mock.ts
+++ src/projects/ui/modules/users/mocks/allUsers.data.mock.ts
@@ -1,7 +1,8 @@
 import type { UserModelDto } from '@shared/dto/models/user.model.dto';
+import { currentUserDataMock } from '@ui/modules/users/mocks/currentUser.data.mock';
 
 export const allUsersDataMock: UserModelDto[] = [
-  { id: 'u0', email: 'admin@example.com', name: 'Administrator' },
+  currentUserDataMock,
   { id: 'u1', email: 'alice.smith@example.com', name: 'Alice Smith' },
   { id: 'u2', email: 'bob.johnson@example.com', name: 'Bob Johnson' },
   { id: 'u3', email: 'charlie.davis@example.com' },
---
+++ src/projects/ui/modules/users/mocks/currentUser.data.mock.ts
@@ -0,0 +1,7 @@
+import type { UserModelDto } from '@shared/dto/models/user.model.dto';
+
+export const currentUserDataMock: UserModelDto = {
+  id: 'u0',
+  email: 'admin@example.com',
+  name: 'Administrator',
+};
