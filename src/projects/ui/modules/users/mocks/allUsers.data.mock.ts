dc304d2641a3c95985358ccc9f2ff3b146e86cc0
vbetsch <vic.betsch@gmail.com>
2025-08-12 20:42:34 +0200

    fix: all users mocks

--- src/projects/ui/modules/users/mocks/allUsers.data.mock.ts
+++ src/projects/ui/modules/users/mocks/allUsers.data.mock.ts
@@ -1,6 +1,7 @@
 import type { UserModelDto } from '@shared/dto/models/user.model.dto';
 
 export const allUsersDataMock: UserModelDto[] = [
+  { id: 'u0', email: 'admin@example.com', name: 'Administrator' },
   { id: 'u1', email: 'alice.smith@example.com', name: 'Alice Smith' },
   { id: 'u2', email: 'bob.johnson@example.com', name: 'Bob Johnson' },
   { id: 'u3', email: 'charlie.davis@example.com' },
