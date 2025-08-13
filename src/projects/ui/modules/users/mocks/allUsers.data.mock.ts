b76f3342cc8009993b1b7901b7dde3b6089d22f4
vbetsch <vic.betsch@gmail.com>
2025-08-14 00:01:04 +0200

    feat: add users

--- src/projects/ui/modules/users/mocks/allUsers.data.mock.ts
+++ src/projects/ui/modules/users/mocks/allUsers.data.mock.ts
@@ -8,4 +8,13 @@ export const allUsersDataMock: UserModelDto[] = [
   { id: 'u3', email: 'charlie.davis@example.com' },
   { id: 'u4', email: 'diana.wilson@example.com', name: 'Diana Wilson' },
   { id: 'u5', email: 'edward.taylor@example.com', name: 'Edward Taylor' },
+  { id: 'u6', email: 'frank.moore@example.com', name: 'Frank Moore' },
+  { id: 'u7', email: 'grace.miller@example.com', name: 'Grace Miller' },
+  { id: 'u8', email: 'henry.anderson@example.com' },
+  { id: 'u9', email: 'isabella.thomas@example.com', name: 'Isabella Thomas' },
+  { id: 'u10', email: 'jackson.white@example.com', name: 'Jackson White' },
+  { id: 'u11', email: 'karen.harris@example.com', name: 'Karen Harris' },
+  { id: 'u12', email: 'liam.martin@example.com' },
+  { id: 'u13', email: 'mia.lee@example.com', name: 'Mia Lee' },
+  { id: 'u14', email: 'nathan.king@example.com', name: 'Nathan King' },
 ];
