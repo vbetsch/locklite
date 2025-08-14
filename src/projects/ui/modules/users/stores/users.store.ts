b153fa8568315059013da982e5a3c77fb652d449
vbetsch <vic.betsch@gmail.com>
2025-08-14 22:42:27 +0200

    feat: create usersStore

---
+++ src/projects/ui/modules/users/stores/users.store.ts
@@ -0,0 +1,16 @@
+import type { IStore } from '@ui/stores/interfaces/store.interface';
+import { createStore } from '@ui/stores/hooks/createStore';
+import type { UserModelDto } from '@shared/modules/users/user.model.dto';
+
+export type UsersStoreState = {
+  allUsers: UserModelDto[];
+  currentUser: UserModelDto | null;
+};
+
+const initialUiState: UsersStoreState = {
+  allUsers: [],
+  currentUser: null,
+};
+
+export const usersStore: IStore<UsersStoreState> =
+  createStore<UsersStoreState>(initialUiState);
