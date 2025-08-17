34662c3d1096f5ba0aa8ee662fc8a851d0a17c56
vbetsch <vic.betsch@gmail.com>
2025-08-18 00:13:46 +0200

    fix: protect api swagger

--- src/app/api/swagger/route.ts
+++ src/app/api/swagger/route.ts
@@ -2,7 +2,11 @@ import { createSwaggerSpec } from 'next-swagger-doc';
 import { NextResponse } from 'next/server';
 import { CONSTANTS } from '@shared/config/constants';
 
-export function GET(): NextResponse {
+export function GET(): NextResponse | null {
+  if (process.env.NODE_ENV === 'production') {
+    return null;
+  }
+
   const spec: object = createSwaggerSpec({
     apiFolder: 'src/app/api',
     schemaFolders: ['src/projects/shared/dto'],
