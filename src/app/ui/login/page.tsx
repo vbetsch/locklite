d41440d219e892fa5e67c2f0b523a576f8f417e3
vbetsch <vic.betsch@gmail.com>
2025-08-17 23:59:37 +0200

    feat: create an alert for privacy in production

--- src/app/ui/login/page.tsx
+++ src/app/ui/login/page.tsx
@@ -5,10 +5,16 @@ import type { JSX } from 'react';
 import PageContainer from '@ui/components/templates/PageContainer';
 import { SignInForm } from '@ui/modules/auth/components/organisms/SignInForm';
 import CircularLoader from '@ui/components/loaders/CircularLoader';
+import { Alert } from '@mui/material';
 
 export default function SignInPage(): JSX.Element {
   return (
     <PageContainer title={'Login'}>
+      <Alert variant="filled" severity="error">
+        {/* eslint-disable-next-line no-restricted-syntax */}
+        This application is <b>fictitious</b>. We strongly recommend that you do
+        not enter your real login details.
+      </Alert>
       <Suspense fallback={<CircularLoader loading />}>
         <SignInForm />
       </Suspense>
