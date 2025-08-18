ecb1ca6ee9e58c5adab3cfc3bfa5cf155c718718
vbetsch <vic.betsch@gmail.com>
2025-08-18 09:37:06 +0200

    fix: try another config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from 'next';
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: false
+  },
+  eslint: {
+    ignoreDuringBuilds: false
+  }
 };
 
 export default nextConfig;
