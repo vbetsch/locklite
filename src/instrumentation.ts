f5f6bb2c5d64cca734093f098db839a250e8ac36
vbetsch <vic.betsch@gmail.com>
2025-08-19 13:13:18 +0200

    fix: instrumentation files not loaded

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,12 +1,13 @@
 import * as Sentry from '@sentry/nextjs';
 
-export async function register() {
+export async function register(): Promise<void> {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./sentry.server.config');
+    await import('../sentry.server.config');
   }
   if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('./sentry.edge.config');
+    await import('../sentry.edge.config');
   }
 }
 
+// eslint-disable-next-line @typescript-eslint/typedef
 export const onRequestError = Sentry.captureRequestError;
