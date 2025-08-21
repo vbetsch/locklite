b7e6f5cba844d4c194aa88f7fca9ff8d747a7022
vbetsch <vic.betsch@gmail.com>
2025-08-22 01:16:39 +0200

    fix: seed

--- src/projects/api/modules/seed/infra/seed.ts
+++ src/projects/api/modules/seed/infra/seed.ts
@@ -1,14 +1,14 @@
 import 'reflect-metadata';
-import { usersToSeed } from '@api/modules/seed/app/data/users.data.seed';
 import prisma from '@lib/prisma';
 import { container } from 'tsyringe';
 import { UpsertUserWithVaultsUseCase } from '@api/modules/seed/domain/upsert-user-with-vaults.usecase';
+import { usersDataSeed } from '@api/modules/seed/app/data/users.data.seed';
 
 const upsertUserWithVaultsUseCase: UpsertUserWithVaultsUseCase =
   container.resolve(UpsertUserWithVaultsUseCase);
 
 async function main(): Promise<void> {
-  for (const seed of usersToSeed) {
+  for (const seed of usersDataSeed) {
     await upsertUserWithVaultsUseCase.handle(seed);
   }
 }
