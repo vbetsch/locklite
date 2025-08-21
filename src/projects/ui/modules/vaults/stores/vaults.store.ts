4e7653b224c804e579031da0c80cc7e52714a297
vbetsch <vic.betsch@gmail.com>
2025-08-21 22:57:53 +0200

    fix: vaults store

--- src/projects/ui/modules/vaults/stores/vaults.store.ts
+++ src/projects/ui/modules/vaults/stores/vaults.store.ts
@@ -2,13 +2,13 @@ import type { IStore } from '@ui/stores/interfaces/store.interface';
 import { createStore } from '@ui/stores/hooks/createStore';
 import type { VaultWithMembersModelDto } from '@shared/modules/vaults/models/vault.with-members.model.dto';
 
-export type UsersStoreState = {
+export type VaultsStoreState = {
   vaults: VaultWithMembersModelDto[];
 };
 
-const initialState: UsersStoreState = {
+const initialState: VaultsStoreState = {
   vaults: [],
 };
 
-export const vaultsStore: IStore<UsersStoreState> =
-  createStore<UsersStoreState>(initialState);
+export const vaultsStore: IStore<VaultsStoreState> =
+  createStore<VaultsStoreState>(initialState);
