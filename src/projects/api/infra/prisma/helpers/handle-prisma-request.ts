81a6a291092509ddbca5f17ef237d53ed977d2ab
vbetsch <vic.betsch@gmail.com>
2025-08-22 21:47:17 +0200

    fix: handle HttpError in handlePrismaRequest

--- src/projects/api/infra/prisma/helpers/handle-prisma-request.ts
+++ src/projects/api/infra/prisma/helpers/handle-prisma-request.ts
@@ -1,5 +1,6 @@
 import { handlePrismaError } from '@api/infra/prisma/helpers/handle-prisma-errors';
 import type { PrismaErrorLike } from '@api/infra/prisma/prisma-error-like.type';
+import { HttpError } from '@shared/errors/http-error';
 
 export async function handlePrismaRequest<T>(
   callback: () => Promise<T>
@@ -7,6 +8,10 @@ export async function handlePrismaRequest<T>(
   try {
     return await callback();
   } catch (error: unknown) {
+    if (error instanceof HttpError) {
+      throw error;
+    }
+
     // if match with type PrismaErrorLike
     if (
       typeof error === 'object' &&
