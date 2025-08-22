4a75e9bef789fd5974d5389bc906b18ccbbe9454
vbetsch <vic.betsch@gmail.com>
2025-08-22 19:06:14 +0200

    docs: complete

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -7,6 +7,32 @@ import type { HttpResponseDto } from '@shared/dto/output/http.response.dto';
 import type { GetUsersListDataDto } from '@shared/modules/users/get-users-list.data.dto';
 import { GetUsersListUseCase } from '@api/modules/users/domain/get-users-list.usecase';
 
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     tags: [Users]
+ *     summary: Get all users
+ *     responses:
+ *       200:
+ *         description: Returns all users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/GetUsersListDataDto'
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/HttpErrorDto'
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/HttpErrorDto'
+ */
 export async function GET(
   request: NextRequest
 ): Promise<NextResponse<HttpResponseDto<GetUsersListDataDto>>> {
