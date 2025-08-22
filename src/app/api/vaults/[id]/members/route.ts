df837d5ec095fff364235c26141c5978ccd69cd1
vbetsch <vic.betsch@gmail.com>
2025-08-22 17:46:26 +0200

    docs: api doc of edit members

--- src/app/api/vaults/[id]/members/route.ts
+++ src/app/api/vaults/[id]/members/route.ts
@@ -3,12 +3,56 @@ import type { HttpOptionsDto } from '@shared/dto/input/http-options.dto';
 import type { EditMembersParamsDto } from '@shared/modules/vaults/endpoints/edit-members/edit-members.params.dto';
 import { container } from 'tsyringe';
 import { handleApiRequest } from '@api/app/handle-api-request';
-import { StatusCodes } from 'http-status-codes';
 import type { HttpResponseDto } from '@shared/dto/output/http.response.dto';
 import type { EditMembersDataDto } from '@shared/modules/vaults/endpoints/edit-members/edit-members.data.dto';
 import type { VaultWithMembersModelDto } from '@shared/modules/vaults/models/vault.with-members.model.dto';
 import type { EditMembersPayloadDto } from '@shared/modules/vaults/endpoints/edit-members/edit-members.payload.dto';
 
+/**
+ * @swagger
+ * /api/vaults/{id}/members:
+ *   put:
+ *     tags: [Vaults]
+ *     summary: Edit members of a vault by ID
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        description: ID of vault to edit
+ *        schema:
+ *          type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/EditMembersPayloadDto'
+ *     responses:
+ *       200:
+ *         description: Returns the vault edited
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/EditMembersDataDto'
+ *       404:
+ *         description: Resource not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/HttpErrorDto'
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/HttpErrorDto'
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/HttpErrorDto'
+ */
 export async function PUT(
   request: NextRequest,
   options: HttpOptionsDto<EditMembersParamsDto>
@@ -27,6 +71,5 @@ export async function PUT(
       };
       return response;
     },
-    successStatusCode: StatusCodes.CREATED,
   });
 }
