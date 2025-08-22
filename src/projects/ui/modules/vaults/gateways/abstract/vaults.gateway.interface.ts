5803c37d113b6e58c81e57358577ebf7b2e2959d
vbetsch <vic.betsch@gmail.com>
2025-08-22 19:27:39 +0200

    feat: implement api calls

--- src/projects/ui/modules/vaults/gateways/abstract/vaults.gateway.interface.ts
+++ src/projects/ui/modules/vaults/gateways/abstract/vaults.gateway.interface.ts
@@ -4,6 +4,10 @@ import type { CreateVaultPayloadDto } from '@shared/modules/vaults/endpoints/cre
 import type { CreateVaultDataDto } from '@shared/modules/vaults/endpoints/create/create-vault.data.dto';
 import type { DeleteVaultParamsDto } from '@shared/modules/vaults/endpoints/delete/delete-vault.params.dto';
 import type { HttpInputDto } from '@shared/dto/input/http-input.dto';
+import type { GetMyVaultsWithMembersDataDto } from '@shared/modules/vaults/endpoints/get-my-vaults/get-my-vaults.with-members.data.dto';
+import type { ShareVaultParamsDto } from '@shared/modules/vaults/endpoints/share-vault/share-vault.params.dto';
+import type { ShareVaultPayloadDto } from '@shared/modules/vaults/endpoints/share-vault/share-vault.payload.dto';
+import type { ShareVaultDataDto } from '@shared/modules/vaults/endpoints/share-vault/share-vault.data.dto';
 
 export interface IVaultsGateway {
   getMyVaults(): Promise<RequestServiceOutputType<GetMyVaultsDataDto>>;
@@ -13,4 +17,11 @@ export interface IVaultsGateway {
   deleteVault(
     input: HttpInputDto<DeleteVaultParamsDto, null>
   ): Promise<RequestServiceOutputType<number>>;
+  getMyVaultsWithMembers(): Promise<
+    RequestServiceOutputType<GetMyVaultsWithMembersDataDto>
+  >;
+  editVaultMembers(input: {
+    params: ShareVaultParamsDto;
+    payload: ShareVaultPayloadDto;
+  }): Promise<RequestServiceOutputType<ShareVaultDataDto>>;
 }
--- src/projects/ui/modules/vaults/gateways/vaults.gateway.ts
+++ src/projects/ui/modules/vaults/gateways/vaults.gateway.ts
@@ -7,6 +7,10 @@ import { DeleteVaultParamsDto } from '@shared/modules/vaults/endpoints/delete/de
 import { CreateVaultPayloadDto } from '@shared/modules/vaults/endpoints/create/create-vault.payload.dto';
 import { IVaultsGateway } from '@ui/modules/vaults/gateways/abstract/vaults.gateway.interface';
 import type { HttpInputDto } from '@shared/dto/input/http-input.dto';
+import { GetMyVaultsWithMembersDataDto } from '@shared/modules/vaults/endpoints/get-my-vaults/get-my-vaults.with-members.data.dto';
+import { ShareVaultParamsDto } from '@shared/modules/vaults/endpoints/share-vault/share-vault.params.dto';
+import { ShareVaultPayloadDto } from '@shared/modules/vaults/endpoints/share-vault/share-vault.payload.dto';
+import { ShareVaultDataDto } from '@shared/modules/vaults/endpoints/share-vault/share-vault.data.dto';
 
 @injectable()
 export class VaultsGateway implements IVaultsGateway {
@@ -37,6 +41,24 @@ export class VaultsGateway implements IVaultsGateway {
   ): Promise<RequestServiceOutputType<number>> {
     return await this._lockliteRequestService.delete<number>(
       '/vaults/' + input.params.vaultId
+    );
+  }
+
+  public async getMyVaultsWithMembers(): Promise<
+    RequestServiceOutputType<GetMyVaultsWithMembersDataDto>
+  > {
+    return await this._lockliteRequestService.get<GetMyVaultsWithMembersDataDto>(
+      '/vaults'
+    );
+  }
+
+  public async editVaultMembers(input: {
+    params: ShareVaultParamsDto;
+    payload: ShareVaultPayloadDto;
+  }): Promise<RequestServiceOutputType<ShareVaultDataDto>> {
+    return await this._lockliteRequestService.put<ShareVaultDataDto>(
+      '/vaults/' + input.params.vaultId,
+      input.payload
     );
   }
 }
